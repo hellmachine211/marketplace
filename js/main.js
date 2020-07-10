fed8a4a1f28c74326622e32054a0a4e178aa1706
hellmachine211 <oskarjohnatan54@gmail.com>
2020-07-10 14:23:55 +0300

    added product-slider and product

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+      $('.product-slider__inner').slick({
+        dots: true,
+        // arrow: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+      });
+
+
     var mixer = mixitup('.products__inner-box');
 
 });
